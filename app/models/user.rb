4c57f4e235656f1524e83fe8dc939987fc4b1a28
Micah Levine <micah.levine@outlook.com>
2020-01-20 20:26:14 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,3 @@
-  class CreateUsers < ActiveRecord::Migration[5.1]
-    def change
-      create_table :users do |t|
-        t.string :name
-        t.string :email
-        t.string :password
-      end
-    end
-
-
+class User < ActiveRecord::Base
+validates_presence_of :name, :email, :password
 end
